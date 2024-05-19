ab6a666224629a84dbb34270c0d0dd75dbb454e2
ItzLoghotXD <121806387+ItzLoghotXD@users.noreply.github.com>
2024-05-19 17:39:15 +0530

    Update and rename theme.js to dark-light-theme.js

---
+++ js/dark-light-theme.js
@@ -0,0 +1,6 @@
+// Toggle between light and dark themes
+const toggleBtn = document.getElementById('themeToggle');
+
+toggleBtn.addEventListener('click', function() {
+    document.body.classList.toggle('light');
+});
